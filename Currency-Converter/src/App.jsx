f83da3fb13fc9ebc26ec228a0c193df6b327aefa
Nirmal Singh <nirmalsingh100754@hotmail.com>
2025-06-21 11:32:40 +0530

    feat: initialize Currency Converter project with React, Tailwind CSS, and Vite
    - Added package.json with dependencies and scripts for development and build
    - Created App component with a basic layout and Tailwind CSS styling
    - Set up main entry point for React application
    - Configured Vite with React and Tailwind CSS plugins
    - Included Tailwind CSS imports in App.css and index.css

---
+++ Currency-Converter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="w-full flex justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+      <h1 className='text-[48px] text-white font-bold'>Currency Converter</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
